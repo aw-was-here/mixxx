e62b0cb686e8be448a74440038aaa39d65273067
David Hernandez <david.hm.1994@gmail.com>
2018-06-20 16:57:58 +0200

    [Untested] Added options to file listener

--- src/preferences/dialog/dlgprefmetadata.h
+++ src/preferences/dialog/dlgprefmetadata.h
@@ -28,7 +28,7 @@ namespace {
             ConfigKey("[Livemetadata]","SettingsChanged");
 
     const bool defaultFileMetadataEnabled = false;
-    const QString defaultEncoding = "UTF-8";
+    const QByteArray defaultEncoding = "UTF-8";
     const QString defaultFileFormat = "SAMBroadcaster";
     const QString defaultFilePath = QDir::currentPath();
     const QString defaultFileFormatString = "author - title";
@@ -36,7 +36,8 @@ namespace {
 
 struct FileSettings {
     bool enabled;
-    QString fileEncoding, fileFormat, fileFormatString, filePath;
+    QByteArray fileEncoding;
+    QString fileFormat, fileFormatString, filePath;
 };
 
 class DlgPrefMetadata : public DlgPreferencePage, public Ui::DlgPrefMetadataDlg {
@@ -44,14 +45,14 @@ class DlgPrefMetadata : public DlgPreferencePage, public Ui::DlgPrefMetadataDlg 
   public:
     DlgPrefMetadata(QWidget *pParent, UserSettingsPointer pSettings);
     ~DlgPrefMetadata() override = default;
-    FileSettings getLatestSettings();
+    static FileSettings getLatestSettings();
+    static FileSettings getPersistedSettings(const UserSettingsPointer &pSettings);
   public slots:
     void slotApply() override;
     void slotCancel() override;
     void slotResetToDefaults() override;
     void slotUpdate() override;
   private:
-    void getPersistedSettings(UserSettingsPointer pSettings);
     void setupWidgets();
     bool fileSettingsDifferent();
     bool checkIfSettingsCorrect();
@@ -61,7 +62,7 @@ class DlgPrefMetadata : public DlgPreferencePage, public Ui::DlgPrefMetadataDlg 
 
     UserSettingsPointer m_pSettings;
     ControlProxy m_CPSettingsChanged;
-    FileSettings m_latestSettings;
+    static FileSettings s_latestSettings;
   private slots:
     void slotFormatChanged(int newIndex);
     void slotFilepathButtonClicked();
